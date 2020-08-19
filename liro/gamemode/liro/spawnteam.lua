d2b17a3ff36985211826749471cbd9fe6e049fc9
Lemons <51181899+LemonsHQ@users.noreply.github.com>
2020-08-18 20:12:02 -0400

    Update spawnteam.lua

---
+++ liro/gamemode/liro/spawnteam.lua
@@ -0,0 +1,10 @@
+function liro:PlayerSpawn( ply )
+    MsgN( ply:Nick() .. " has spawned!" )
+end
+
+local function Assign( ply )
+    print( ply:Nick().. " has spawned and has been assigned recruit!")
+    
+    
+end
+hook.Add( "PlayerSpawn", "some_unique_name", Spawn )
